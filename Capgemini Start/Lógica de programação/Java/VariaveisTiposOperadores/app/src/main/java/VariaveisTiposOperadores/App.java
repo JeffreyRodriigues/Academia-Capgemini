68318cc95d74e60fb56d702955327a8c05caff90
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-03-17 19:35:15 -0300

    Exploring IDE NEtBeans

---
+++ Capgemini Start/Lógica de programação/Java/VariaveisTiposOperadores/app/src/main/java/VariaveisTiposOperadores/App.java
@@ -0,0 +1,18 @@
+
+package VariaveisTiposOperadores;
+
+public class App {
+
+
+    public static void main(String[] args) {
+ 
+        int idade = 0;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d;
+        char genero = 'M';
+        byte ponto = 0;
+        boolean estaCadastrado = false;
+        
+        String nome = "Esse é um texto de uma variável String";
+    }
+}
