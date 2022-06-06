47fdb0633f6f37d032d6464d714b792077f4bd80
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-06 12:20:04 -0300

    Initial commit router

---
+++ Capgemini Start/Angular/roteamento/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+
+const routes: Routes = [
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent },
+  { path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
