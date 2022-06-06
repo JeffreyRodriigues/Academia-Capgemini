adf02ef06c76492b14c62bb872f47d44ffa9a53e
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-06 12:28:54 -0300

    add Lazy-Loading
    Load just necessary archives of pages

--- Capgemini Start/Angular/roteamento/src/app/app-routing.module.ts
+++ Capgemini Start/Angular/roteamento/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.componen
 const routes: Routes = [
   { path: "primeira-pagina", component: PrimeiraPaginaComponent },
   { path: "segunda-pagina", component: SegundaPaginaComponent },
-  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent }
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }
 ]
 
 @NgModule({
