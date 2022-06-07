4d237b594e9b7ac5dfeab880169471c214fafc5b
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-07 10:57:45 -0300

    create Product services

--- Capgemini Start/Angular/PROWAY-COMPUTERS/src/app/produtos.ts
+++ Capgemini Start/Angular/PROWAY-COMPUTERS/src/app/produtos.ts
@@ -3,23 +3,24 @@ export interface IProduto {
   descricao: string;
   preco: number;
   descricaoPreco: string;
+  quantidadeEstoque: number;
   imagem: string;
 }
 
-export const produtos = [
-  { id: 1, descricao: "Mouse gamer", preco: 439.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-gamer.jpg" },
-  { id: 2, descricao: "Monitor gamer", preco: 1339.50, descricaoPreco: "À vista no PIX", imagem: "/assets/monitor-gamer.jpg" },
-  { id: 3, descricao: "Teclado mecânico", preco: 769.00, descricaoPreco: "À vista no PIX", imagem: "/assets/teclado-mecanico.jpg" },
-  { id: 4, descricao: "Headphone gamer", preco: 599.00, descricaoPreco: "À vista no PIX", imagem: "/assets/headphone-gamer.jpg" },
-  { id: 5, descricao: "Fone de ouvido", preco: 199.00, descricaoPreco: "À vista no PIX", imagem: "/assets/fone-de-ouvido.jpg" },
-  { id: 6, descricao: "Fone de ouvido Apple", preco: 939.00, descricaoPreco: "À vista no PIX", imagem: "/assets/fone-de-ouvido-apple.jpg" },
-  { id: 7, descricao: "HD 1TB", preco: 499.00, descricaoPreco: "À vista no PIX", imagem: "/assets/hd-1tb.jpg" },
-  { id: 8, descricao: "Combo de Placa de Vídeo", preco: 18939.00, descricaoPreco: "À vista no PIX", imagem: "/assets/combo-placa.jpg" },
-  { id: 9, descricao: "Processador Ryzen", preco: 1000.00, descricaoPreco: "À vista no PIX", imagem: "/assets/processador-ryzen.jpg" },
-  { id: 10, descricao: "Notebook profissional", preco: 2539.00, descricaoPreco: "À vista no PIX", imagem: "/assets/notebook-profissional.jpg" },
-  { id: 11, descricao: "Notebook gamer", preco: 4599.00, descricaoPreco: "À vista no PIX", imagem: "/assets/notebook-gamer.jpg" },
-  { id: 12, descricao: "Mouse simples", preco: 29.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-simples.jpg" },
-  { id: 13, descricao: "Mouse profissional", preco: 239.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-profissional.jpg" },
-  { id: 13, descricao: "Mouse sem fio", preco: 69.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-semfio.jpg" },
-  { id: 13, descricao: "Teclado profissional", preco: 239.00, descricaoPreco: "À vista no PIX", imagem: "/assets/teclado-profissional.jpg" }
+export const produtos: IProduto[] = [
+  { id: 1, descricao: "Mouse gamer", preco: 439.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-gamer.jpg", quantidadeEstoque: 30 },
+  { id: 2, descricao: "Monitor gamer", preco: 1339.50, descricaoPreco: "À vista no PIX", imagem: "/assets/monitor-gamer.jpg", quantidadeEstoque: 15 },
+  { id: 3, descricao: "Teclado mecânico", preco: 769.00, descricaoPreco: "À vista no PIX", imagem: "/assets/teclado-mecanico.jpg", quantidadeEstoque: 10 },
+  { id: 4, descricao: "Headphone gamer", preco: 599.00, descricaoPreco: "À vista no PIX", imagem: "/assets/headphone-gamer.jpg", quantidadeEstoque: 10 },
+  { id: 5, descricao: "Fone de ouvido", preco: 199.00, descricaoPreco: "À vista no PIX", imagem: "/assets/fone-de-ouvido.jpg", quantidadeEstoque: 10 },
+  { id: 6, descricao: "Fone de ouvido Apple", preco: 939.00, descricaoPreco: "À vista no PIX", imagem: "/assets/fone-de-ouvido-apple.jpg", quantidadeEstoque: 10 },
+  { id: 7, descricao: "HD 1TB", preco: 499.00, descricaoPreco: "À vista no PIX", imagem: "/assets/hd-1tb.jpg", quantidadeEstoque: 10 },
+  { id: 8, descricao: "Combo de Placa de Vídeo", preco: 18939.00, descricaoPreco: "À vista no PIX", imagem: "/assets/combo-placa.jpg", quantidadeEstoque: 10 },
+  { id: 9, descricao: "Processador Ryzen", preco: 1000.00, descricaoPreco: "À vista no PIX", imagem: "/assets/processador-ryzen.jpg", quantidadeEstoque: 10 },
+  { id: 10, descricao: "Notebook profissional", preco: 2539.00, descricaoPreco: "À vista no PIX", imagem: "/assets/notebook-profissional.jpg", quantidadeEstoque: 10 },
+  { id: 11, descricao: "Notebook gamer", preco: 4599.00, descricaoPreco: "À vista no PIX", imagem: "/assets/notebook-gamer.jpg", quantidadeEstoque: 10 },
+  { id: 12, descricao: "Mouse simples", preco: 29.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-simples.jpg", quantidadeEstoque: 10 },
+  { id: 13, descricao: "Mouse profissional", preco: 239.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-profissional.jpg", quantidadeEstoque: 10 },
+  { id: 13, descricao: "Mouse sem fio", preco: 69.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-semfio.jpg", quantidadeEstoque: 10 },
+  { id: 13, descricao: "Teclado profissional", preco: 239.00, descricaoPreco: "À vista no PIX", imagem: "/assets/teclado-profissional.jpg", quantidadeEstoque: 10 }
 ]
