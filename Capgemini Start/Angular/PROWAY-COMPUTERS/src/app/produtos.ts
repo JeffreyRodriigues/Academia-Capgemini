10fbc49cf9f0cb79e9545548904ce13fd02e63d2
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-09 14:04:09 -0300

    Create a contact page

--- Capgemini Start/Angular/PROWAY-COMPUTERS/src/app/produtos.ts
+++ Capgemini Start/Angular/PROWAY-COMPUTERS/src/app/produtos.ts
@@ -12,7 +12,7 @@ export interface IProdutoCarrinho extends IProduto {
 }
 
 export const produtos: IProduto[] = [
-  { id: 1, descricao: "Mouse gamer", preco: 439.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-gamer.jpg", quantidadeEstoque: 2 },
+  { id: 1, descricao: "Mouse gamer", preco: 439.00, descricaoPreco: "À vista no PIX", imagem: "/assets/mouse-gamer.jpg", quantidadeEstoque: 20 },
   { id: 2, descricao: "Monitor gamer", preco: 1339.50, descricaoPreco: "À vista no PIX", imagem: "/assets/monitor-gamer.jpg", quantidadeEstoque: 15 },
   { id: 3, descricao: "Teclado mecânico", preco: 769.00, descricaoPreco: "À vista no PIX", imagem: "/assets/teclado-mecanico.jpg", quantidadeEstoque: 10 },
   { id: 4, descricao: "Headphone gamer", preco: 599.00, descricaoPreco: "À vista no PIX", imagem: "/assets/headphone-gamer.jpg", quantidadeEstoque: 10 },
