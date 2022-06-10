665eccd868eddbca999306ed2133d354081e97ee
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-10 14:11:37 -0300

    add examples Java

---
+++ Capgemini Start/Java/Java-Fundamentos/Literais/src/Literal.java
@@ -0,0 +1,22 @@
+
+public class Literal {
+
+	public static void main(String[] args) {
+		int numero;
+		String cadeiaCaracteres;
+		char caracter;
+		boolean certoOuErrado;
+		
+		numero = 10;
+		cadeiaCaracteres = "Meu Curso de Java Fundamentos";
+		caracter = 'S';
+		certoOuErrado = true;
+		
+		System.out.println(" O valor de numéro é: " + numero);
+		System.out.println(" O valor de cadeiaCaracteres é: " + numero);
+		System.out.println(" O valor de caracter é: " + numero);
+		System.out.println(" O valor de certoOuErrado é: " + numero);
+
+	}
+
+}
