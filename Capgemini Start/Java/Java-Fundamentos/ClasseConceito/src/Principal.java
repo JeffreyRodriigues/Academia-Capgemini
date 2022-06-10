27c98ec2007d37935e844860f0c7c510e390f790
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-10 19:48:00 -0300

    Finished Module COmpletely

---
+++ Capgemini Start/Java/Java-Fundamentos/ClasseConceito/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Pessoa pes = new Pessoa();
+		pes.setNomePessoa("JOÃO DO NASCIMENTO");
+		pes.setIdadePessoa(15);
+		System.out.println(pes.toString());
+
+	}
+
+}
