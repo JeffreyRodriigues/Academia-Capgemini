57e9dbaa95cf474ae48ed2b62b958dd21b97c860
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-06-10 12:28:32 -0300

    init Java-Fundamentos

---
+++ Capgemini Start/Java/Java-Fundamentos/MeuPrimeiroProg/src/ClassePrincipal.java
@@ -0,0 +1,12 @@
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		final float numero = 155.5f;
+
+		System.out.println("Valor de número é : "+ numero);
+		
+
+	}
+
+}
---
+++ Capgemini Start/Java/Java-Fundamentos/TiposDeDados/src/TipoDado.java
@@ -0,0 +1,25 @@
+
+public class TipoDado {
+
+	public static void main(String[] args) {
+		// Demonstração de tipos de dados
+		int numero;
+		float decimalCurto;
+		double decimalLongo;
+		char caracter;
+		String cadeiaCaracteres;
+		
+		numero = 10;
+		decimalCurto = 10.5f;
+		decimalLongo = 20.5;
+		caracter = 'A';
+		cadeiaCaracteres = "** Meu Curso de Java Fundamentos 2022 **";
+		
+		System.out.println(numero);
+		System.out.println(decimalCurto);
+		System.out.println(decimalLongo);
+		System.out.println(caracter);
+		System.out.println(cadeiaCaracteres);
+	}
+
+}
